82f41cf0041a8dc10e72f3a545ef541b91cddc8f
tomino2001 <83927837+tomino2001@users.noreply.github.com>
2021-11-20 18:19:35 +0200

    Rezolvat cerinta 2

--- src/main/java/socialnetwork/service/UtilizatoriPrieteniiService.java
+++ src/main/java/socialnetwork/service/UtilizatoriPrieteniiService.java
@@ -1,10 +1,19 @@
 package socialnetwork.service;
 
+import jdk.vm.ci.meta.Local;
+import socialnetwork.domain.Prietenie;
+import socialnetwork.domain.Tuple;
 import socialnetwork.domain.Utilizator;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 public class UtilizatoriPrieteniiService {
     private final UtilizatorService utilizatorService;
@@ -18,5 +27,29 @@ public class UtilizatoriPrieteniiService {
     public void removeUtilizatorAndPrieteniiUtilizator(Long id){
         this.utilizatorService.removeUtilizator(id);
         this.prietenieService.removePreteniiIfUserIsDeleted(id);
+    }
+
+    public List<Tuple<Long, LocalDateTime>> prieteniiUtilizatorDinLuna(String firstName, String lastName, String luna){
+        Utilizator utilizator = utilizatorService.findByName(firstName, lastName);
+        Iterable<Prietenie> prietenii = prietenieService.getAll();
+
+        List<Tuple<Long, LocalDateTime>> rezultat =
+                StreamSupport.stream(prietenii.spliterator(), false)
+                .filter(prietenie -> (prietenie.getId().getLeft() == utilizator.getId()
+                                || Objects.equals(prietenie.getId().getRight(), utilizator.getId()))
+                            && prietenie.getDate().getMonth() == Month.valueOf(luna.toUpperCase()))
+                .map(prietenie -> {
+                    Long left = prietenie.getId().getLeft();
+                    Long right = prietenie.getId().getRight();
+                    Long idUser = utilizator.getId();
+                    Tuple<Long, LocalDateTime> tuple = null;
+                    if (left != idUser)
+                        tuple = new Tuple<>(left, prietenie.getDate());
+                    else
+                        tuple = new Tuple<>(right, prietenie.getDate());
+                    return tuple;
+                })
+                .collect(toList());
+        return rezultat;
     }
 }
